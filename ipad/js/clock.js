84a2ba4febf0aef33aefb23f52a23215be7cd3f6
DESKTOP-GJDVO17\xvz17 <xvz1704@naver.com>
2025-01-02 23:06:36 +0900

    집

--- ipad/js/clock.js
+++ ipad/js/clock.js
@@ -1,6 +1,12 @@
 function clockRun() {
     const ipadClock = document.querySelector(".ipad-clock");
     setInterval(() => {
-        const now = 
-    })
+        const now = new Date();
+        const nowHours = now.getHours();
+        const nowMinutes = now.getMinutes() < 10 ? `0${now.getMinutes()}` : now.getMinutes();
+        const nowClockText = `${nowHours}:${nowMinutes}`;
+        ipadClock.innerHTML = nowClockText;
+    }, 1000);
 }
+
+clockRun();
